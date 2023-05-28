c2585bdfe5b70c03bfa12e5060a8a5b28307b162
Abdalrhmanabood <abdalrhmanabood16@gmail.com>
2023-05-28 11:24:27 +0200

    add fields to note button sheet

--- lib/views/widget/add_note_botton_sheet.dart
+++ lib/views/widget/add_note_botton_sheet.dart
@@ -7,13 +7,22 @@ class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-       padding:const  EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: const [
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(
+            hint: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+           CustomTextField(
+            hint: 'Content',
+            maxlines: 5,
+          ),
         ],
       ),
     );
--- lib/views/widget/custom_text_field.dart
+++ lib/views/widget/custom_text_field.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint, this.maxlines = 1});
+  final String hint;
+  final int maxlines;
   @override
   Widget build(BuildContext context) {
     return TextField(
-      cursorColor: kPrimaryColor ,
+      cursorColor: kPrimaryColor,
+      maxLength: maxlines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         // hintStyle: const TextStyle(
         //   color: kPrimaryColor ,
         // ),
@@ -21,11 +23,10 @@ class CustomTextField extends StatelessWidget {
   }
 
   OutlineInputBorder buildBorder([color]) {
-    return  OutlineInputBorder(
+    return OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
-        borderSide:  BorderSide(
+        borderSide: BorderSide(
           color: color ?? Colors.white,
-        )
-      );
+        ));
   }
 }
