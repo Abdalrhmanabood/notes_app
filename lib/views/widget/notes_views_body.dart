48b3513aba030c141bd014010516121c2bc76f4a
Abdalrhmanabood <abdalrhmanabood16@gmail.com>
2023-05-29 20:21:11 +0200

    Display data part one

--- lib/views/widget/notes_views_body.dart
+++ lib/views/widget/notes_views_body.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/views/widget/notes_list_view.dart';
+import '../../cubits/notes_cubit/notes_cubit.dart';
 import 'custom_app_bar.dart';
 
-class NotesViewsBody extends StatelessWidget {
+class NotesViewsBody extends StatefulWidget {
   const NotesViewsBody({super.key});
+
+  @override
+  State<NotesViewsBody> createState() => _NotesViewsBodyState();
+}
+
+class _NotesViewsBodyState extends State<NotesViewsBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 24),
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children:  [
+        children: [
           SizedBox(height: 50),
           CustomAppBar(
             title: 'Notes',
             icon: Icons.search,
           ),
-         Expanded(child: NotesListView(), ) 
+          Expanded(
+            child: NotesListView(),
+          )
         ],
       ),
     );
   }
 }
-
